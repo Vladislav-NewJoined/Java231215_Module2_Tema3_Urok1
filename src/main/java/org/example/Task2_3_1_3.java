ef26c9d0c34c8bbb0e290de938a172373413f298
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-12-15 21:30:26 +0300

    Initial commit

---
+++ src/main/java/org/example/Task2_3_1_3.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Task2_3_1_3 {
+    public static void main(String[] args) {
+        System.out.println("""
+            Задание:\s
+            Модуль 2. Тема 3. Урок 1. Maven, Gradle
+                Задание 3. При помощи Maven создайте свой первый проект в нем создайте
+                класс Main  и напишите программу которая будет выводить сообщение
+                “Это мой первый проект Maven!"
+
+            Решение:\s""");
+
+
+        System.out.println("Это мой первый проект Maven!");
+    }
+}
